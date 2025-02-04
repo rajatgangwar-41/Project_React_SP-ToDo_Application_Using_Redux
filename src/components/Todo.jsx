f14bef86db9d2e780f419d1bec0b41e82a1e730b
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-02-04 19:26:48 +0530

    Implemented useSelector hook to use the states in components

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,14 @@
+import { useSelector } from 'react-redux'
+import { MdDeleteForever } from 'react-icons/md'
+
 const Todo = () => {
+
+  const tasks = useSelector(state => state.task)
+
+  const handleTaskDelete = () => {
+    return {};
+  }
+
   return (
     <div className="container">
       <div className="todo-app">
@@ -11,7 +21,25 @@ const Todo = () => {
             <button>Add Task</button>
           </form>
         </div>
-        <ul id="list-container"></ul>
+        <ul id="list-container">
+          {
+            tasks.map((curTask, index) => {
+              return (
+                <li key={index}>
+                  <p>
+                    {index}: {curTask}
+                  </p>
+                  <div>
+                    <MdDeleteForever
+                      className="icon-style"
+                      onClick={() => handleTaskDelete(index)}
+                    />
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     </div>
   )
